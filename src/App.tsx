30bd15ceba4c75c1c0ce07f424929ab14119f5e0
SaydAliUsmanov <usmanov.guno@mail.ru>
2022-12-25 00:37:20 +0300

    Исправил названия контекста темы

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,11 @@ import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss'
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
-import { Theme, ThemeContent } from './theme/ThemeContext';
 import { useTheme } from './theme/useTheme';
 
 const App = () => {
 	const {theme, toggleTheme} = useTheme()
-	
+
   return (
     <div className={`app ${theme}`}>
 			<button onClick={toggleTheme}>ToggleTheme</button>
