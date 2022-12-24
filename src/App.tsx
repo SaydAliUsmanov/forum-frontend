840de535ce45f6b2cbb5a100edf1f437fccb6137
SaydAliUsmanov <usmanov.guno@mail.ru>
2022-12-25 00:20:03 +0300

    Настроены базовые переменные стилей и темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Suspense } from 'react';
+import { Suspense, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss'
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import { Theme, ThemeContent } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+	const {theme, toggleTheme} = useTheme()
+	
   return (
-    <div>
+    <div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>ToggleTheme</button>
       <Link to={'/'}>Перейти на главаную</Link>
       <Link to={'/about'}>Перейти на Об странице</Link>
       <Suspense fallback={<div>Loading...</div>}>
