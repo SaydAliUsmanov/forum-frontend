f264dff0cafbc64eb39c3f0e116597957f38526c
SaydAliUsmanov <usmanov.guno@mail.ru>
2023-01-03 12:25:42 +0300

    Добавил функцию classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Suspense, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import { classNames } from './helpers/classNames/classNames'
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
 import './styles/index.scss'
-import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
-import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
-import { useTheme } from './theme/useTheme';
+import { useTheme } from './theme/useTheme'
 
 const App = () => {
-	const {theme, toggleTheme} = useTheme()
+  const { theme, toggleTheme } = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
-			<button onClick={toggleTheme}>ToggleTheme</button>
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>ToggleTheme</button>
       <Link to={'/'}>Перейти на главаную</Link>
       <Link to={'/about'}>Перейти на Об странице</Link>
       <Suspense fallback={<div>Loading...</div>}>
@@ -21,7 +21,7 @@ const App = () => {
         </Routes>
       </Suspense>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
